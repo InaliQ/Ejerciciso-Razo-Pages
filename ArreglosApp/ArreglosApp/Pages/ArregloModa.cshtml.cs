19694b0fab32a77bdca80cd158e5cd0d1c9f7541
InaliQ <hernandez.medi.eduardo19@gmail.com>
2024-05-16 11:07:50 -0600

    Finalizacion de trabajo

---
+++ ArreglosApp/ArreglosApp/Pages/ArregloModa.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArreglosApp.Pages
+{
+    public class ArregloModaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
