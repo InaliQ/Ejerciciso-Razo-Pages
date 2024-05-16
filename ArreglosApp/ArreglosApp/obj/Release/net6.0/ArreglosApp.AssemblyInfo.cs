71b877c37c3ba02843cf05e689b4064e9aa235ab
InaliQ <hernandez.medi.eduardo19@gmail.com>
2024-05-16 11:23:24 -0600

    Final

--- ArreglosApp/ArreglosApp/obj/Release/net6.0/ArreglosApp.AssemblyInfo.cs
+++ ArreglosApp/ArreglosApp/obj/Release/net6.0/ArreglosApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArreglosApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19694b0fab32a77bdca80cd158e5cd0d1c9f7541")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArreglosApp")]
